98eb38898ab94750a115731d83b2c744681c98a5
farewell7117 <farewell7117@gmail.com>
2019-09-16 20:58:04 +0100

    Make init and update methods "atomic" (@ref https://clojure.org/reference/atoms).
    Return atom as result.

--- main.cljs
+++ main.cljs
@@ -3,23 +3,25 @@
 (defn init-team
     [teams id]
     "Creates a taxi team"
-    (assoc teams (keyword id) {
+    (atom (swap! teams assoc (keyword id) {
         :from ""
         :to ""
         :time 0
         :cost 0
         :messages []
         :passengers []
-    })
+    }))
 )
 
 (defn update-team
     [teams id new-team]
     "Updates an existing team"
     (let [id-keyword (keyword id)]
-        (-> teams
-            (assoc id-keyword (merge (get teams id-keyword) new-team))
-        )
+        (atom (-> teams
+            (swap! assoc id-keyword (
+                merge (get @teams id-keyword) new-team
+            ))
+        ))
     )
 )
 
@@ -27,7 +29,7 @@
     [teams id name]
     "Join existing team"
     (let [id-keyword (keyword id)
-          team (get teams id-keyword)
+          team (get @teams id-keyword)
           passengers (get team :passengers)
           new-passengers (conj passengers name)]
         (-> teams
@@ -40,7 +42,7 @@
     [teams id message]
     "Send message to the team"
     (let [id-keyword (keyword id)
-          team (get teams id-keyword)
+          team (get @teams id-keyword)
           messages (get team :messages)
           new-messages (conj messages message)]
         (-> teams
@@ -49,15 +51,32 @@
     )
 )
 
-(print (init-team {} "123456789"))
+; Some tests
+(print (init-team (atom {}) "123456789"))
 (print "\n")
-(print (update-team (init-team {} "123456789") "123456789" {:from "Skoltech" :to "Moscow"}))
-(print "\n")
-(print (join-team (update-team (init-team {} "123456789") "123456789" {:from "Skoltech" :to "Moscow"}) "123456789" "Artem"))
-(print "\n")
-(print (-> (send-message (update-team (init-team {} "123456789") "123456789" {:from "Skoltech" :to "Moscow"}) "123456789" "Hello, World!")
-           (send-message "123456789" "Hello, Artem!")
-           (send-message "123456789" "What's the deadline for HW1?")
-           (send-message "123456789" "It has expired already!")
+
+(print (-> (init-team (atom {}) "123456789")
+           (update-team "123456789" {:from "Skoltech" :to "Moscow"})
        )
 )
+(print "\n")
+
+(print (-> (init-team (atom {}) "123456789")
+           (update-team "123456789" {:from "Skoltech" :to "Moscow"})
+           (join-team "123456789" "Artem")
+           (join-team "123456789" "Serge")
+       )
+)
+(print "\n")
+
+(print (-> (init-team (atom {}) "123456789")
+           (update-team "123456789" {:from "Skoltech" :to "Moscow"})
+           (join-team "123456789" "Artem")
+           (join-team "123456789" "Serge")
+           (send-message "123456789" "Serge: Hello, Artem!")
+           (send-message "123456789" "Serge: What's the deadline for HW1?")
+           (send-message "123456789" "Artem: It has expired already...")
+           (send-message "123456789" "Serge: F*ck!")
+       )
+)
+(print "\n")
